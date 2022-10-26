adc54919e8d4648cffcb89e633916447a0f3327c
RoggerK <at76324897@idat.edu.pe>
2022-10-25 19:17:57 -0500

    init ec2

---
+++ src/main/java/idat/edu/pe/sw2/bodega/kennedy/curisinche/Ec2KennedyCurisincheBodegaApplication.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.sw2.bodega.kennedy.curisinche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2KennedyCurisincheBodegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2KennedyCurisincheBodegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idat/edu/pe/sw2/bodega/kennedy/curisinche/Ec2KennedyCurisincheBodegaApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.sw2.bodega.kennedy.curisinche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2KennedyCurisincheBodegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
